44a040b13a2d27ac50f3824a31c59ebb4bfb8887
ramsagar2410 <ramsagar2410@gmail.com>
2023-06-21 14:34:02 +0530

    integrated Discovery and API Gateway (#2)
    * Fixed build issues
    
    Fixed build issues due to missing database mocks.
    
    * integrated with discovery server
    
    * Created an API gateway module

--- inventory-service/src/test/java/commerce/platform/inventoryservice/InventoryServiceApplicationTests.java
+++ inventory-service/src/test/java/commerce/platform/inventoryservice/InventoryServiceApplicationTests.java
@@ -1,10 +1,29 @@
 package commerce.platform.inventoryservice;
 
+import ch.qos.logback.core.net.SyslogOutputStream;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
 
 @SpringBootTest
 class InventoryServiceApplicationTests {
+
+	@Container
+	static MySQLContainer mySQLContainer = new MySQLContainer("mysql:8.0");
+
+	static {
+		mySQLContainer.withDatabaseName("inventory-service").start();
+	}
+
+	@DynamicPropertySource
+	static void setProperties(DynamicPropertyRegistry dymDynamicPropertyRegistry) {
+		dymDynamicPropertyRegistry.add("spring.datasource.url", mySQLContainer::getJdbcUrl);
+		dymDynamicPropertyRegistry.add("spring.datasource.username",mySQLContainer::getUsername);
+		dymDynamicPropertyRegistry.add("spring.datasource.password",mySQLContainer::getPassword);
+	}
 
 	@Test
 	void contextLoads() {
